07a7315e042d0d4fb63da16f6fa45b141b1505df
goda-kazuki <goda@eyemovic.com>
2020-10-18 20:22:56 +0900

    82 スピナー表示。ダークモード強制オフ

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -8,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        ダークモードを拒否
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
