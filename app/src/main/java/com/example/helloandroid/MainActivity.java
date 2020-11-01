7bc849f8c571033afb388adb7e09b46679f53a6e
goda-kazuki <goda@eyemovic.com>
2020-11-01 15:27:22 +0900

    98 ボタンでテキストが更新されるところまで

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btClick=findViewById(R.id.btClick);
+        HelloListener helloListener=new HelloListener();
+
+        btClick.setOnClickListener(helloListener);
+    }
+
+    private class HelloListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            EditText input=findViewById(R.id.etName);
+
+            TextView output=findViewById(R.id.tvOutput);
+
+            String inputStr=input.getText().toString();
+
+            output.setText(inputStr + "さん、こんにちは");
+        }
     }
 }
