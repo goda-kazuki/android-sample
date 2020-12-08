ca965863377b8ee807a4d8f8555312fed74f8c64
合田和樹 <goda@eyemovic.com>
2020-12-09 08:51:43 +0900

    113 ListAdapterを使用

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,25 +20,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView lvMenu = findViewById(R.id.lvMenu);
-        lvMenu.setOnItemClickListener(new ListItemClickListener());
-    }
 
-    private static Toast toast;
+        List<String> menuList = new ArrayList<>();
 
-    private class ListItemClickListener implements AdapterView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            String item = (String) parent.getItemAtPosition(position);
+        menuList.add(" から揚げ定食 ");
+        menuList.add(" ハンバーグ定食 ");
+        menuList.add(" 生姜焼き定食 ");
+        menuList.add(" ステーキ定食 ");
+        menuList.add(" 野菜炒め定食 ");
+        menuList.add(" とんかつ定食 ");
+        menuList.add(" ミンチかつ定食 ");
+        menuList.add(" チキンカツ定食 ");
+        menuList.add(" コロッケ定食 ");
+        menuList.add(" 焼き魚定食 ");
+        menuList.add(" 焼肉定食 ");
 
-            String show = "あなたが選んだ定食" + item;
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                menuList
+        );
 
-            // Toastが重複されないように調整する
-            if (toast != null) {
-                toast.cancel();
-            }
-
-            toast = Toast.makeText(MainActivity.this, show, Toast.LENGTH_SHORT);
-            toast.show();
-        }
+        lvMenu.setAdapter(adapter);
     }
 }
