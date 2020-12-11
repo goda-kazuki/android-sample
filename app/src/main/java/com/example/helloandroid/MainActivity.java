55f61ae04576d5b2af3089cf4a1f35a63c2fc90e
合田和樹 <goda@eyemovic.com>
2020-12-11 10:09:18 +0900

    ConstraintLayoutが動くように調整

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -4,13 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,39 +15,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<String> menuList = new ArrayList<>();
-        menuList.add(" から揚げ定食 ");
-        menuList.add(" ハンバーグ定食 ");
-        menuList.add(" 生姜焼き定食 ");
-        menuList.add(" ステーキ定食 ");
-        menuList.add(" 野菜炒め定食 ");
-        menuList.add(" とんかつ定食 ");
-        menuList.add(" ミンチかつ定食 ");
-        menuList.add(" チキンカツ定食 ");
-        menuList.add(" コロッケ定食 ");
-        menuList.add(" 焼き魚定食 ");
-        menuList.add(" 焼肉定食 ");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                MainActivity.this,
-                android.R.layout.simple_list_item_1,
-                menuList
-        );
-
-        ListView lvMenu = findViewById(R.id.lvMenu);
-        lvMenu.setAdapter(adapter);
-
-        // リストビューにリスナを設定。
-        lvMenu.setOnItemClickListener(new ListItemClickListener());
-    }
-
-    private class ListItemClickListener implements AdapterView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            // 注文確認ダイアログフラグメントオブジェクトを生成。
-            OrderConfirmDialogFragment dialogFragment = new OrderConfirmDialogFragment();
-            // ダイアログ表示。
-            dialogFragment.show(getSupportFragmentManager(), "OrderConfirmDialogFragment");
-        }
     }
 }
