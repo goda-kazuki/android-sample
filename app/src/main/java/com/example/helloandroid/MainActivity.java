03555c732b6586af3bf248ed5b9b91a708d64a2b
合田和樹 <goda@eyemovic.com>
2020-12-16 09:28:11 +0900

    163 画面遷移ができるようになったところまで

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,17 +3,70 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        ダークモードを拒否
+        // ダークモードを拒否
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 画面部品ListViewを取得。
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        // SimpleAdapterで使用するListオブジェクトを用意。
+        List<Map<String, String>> menuList = new ArrayList<>();
+        Map<String, String> menu = new HashMap<>();
+        menu.put("name", "から揚げ定食");
+        menu.put("price", "800円");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "ハンバーグ定食");
+        menu.put("price", "850円");
+        menuList.add(menu);
+        // SimpleAdapter第4引数from用データの用意。
+        String[] from = {"name", "price"};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+        SimpleAdapter adapter = new SimpleAdapter(
+                MainActivity.this,
+                menuList,
+                android.R.layout.simple_list_item_2,
+                from,
+                to);
+        lvMenu.setAdapter(adapter);
+
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // タップされた行のデータを取得。SimpleAdapterでは1行分のデータはMap型!
+            Map<String, String> item = (Map<String, String>) parent.getItemAtPosition(position);
+            // 定食名と金額を取得。
+            String menuName = item.get("name");
+            String menuPrice = item.get("price");
+
+            // インテントオブジェクトを生成。
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+            intent.putExtra("menuName", menuName);
+            intent.putExtra("menuPrice", menuPrice);
+            // 第2画面の起動。
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/helloandroid/MainActivity2.java
@@ -0,0 +1,33 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        // インテントオブジェクトを取得。
+        Intent intent = getIntent();  // リスト画面から渡されたデータを取得。
+        String menuName = intent.getStringExtra("menuName");
+        String menuPrice = intent.getStringExtra("menuPrice");
+
+        // 定食名と金額を表示させるTextViewを取得。
+        TextView tvMenuName = findViewById(R.id.tvMenuName);
+        TextView tvMenuPrice = findViewById(R.id.tvMenuPrice);
+        // TextViewに定食名と金額を表示。
+        tvMenuName.setText(menuName);
+        tvMenuPrice.setText(menuPrice);
+    }
+
+    public void onBackButtonClick(View view) {
+        finish();
+    }
+}
