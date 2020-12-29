b546494f52204c5be540370081f0f44af8850dd4
合田和樹 <goda@eyemovic.com>
2020-12-29 20:38:06 +0900

    174 intentの動作を確認

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -16,57 +17,56 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // ダークモードを拒否
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
+        Log.i("LifeCycleSample", "Main onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // 画面部品ListViewを取得。
-        ListView lvMenu = findViewById(R.id.lvMenu);
-        // SimpleAdapterで使用するListオブジェクトを用意。
-        List<Map<String, String>> menuList = new ArrayList<>();
-        Map<String, String> menu = new HashMap<>();
-        menu.put("name", "から揚げ定食");
-        menu.put("price", "800円");
-        menuList.add(menu);
-        menu = new HashMap<>();
-        menu.put("name", "ハンバーグ定食");
-        menu.put("price", "850円");
-        menuList.add(menu);
-        // SimpleAdapter第4引数from用データの用意。
-        String[] from = {"name", "price"};
-        int[] to = {android.R.id.text1, android.R.id.text2};
-        SimpleAdapter adapter = new SimpleAdapter(
-                MainActivity.this,
-                menuList,
-                android.R.layout.simple_list_item_2,
-                from,
-                to);
-        lvMenu.setAdapter(adapter);
-
-        lvMenu.setOnItemClickListener(new ListItemClickListener());
     }
 
-    private class ListItemClickListener implements AdapterView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            // タップされた行のデータを取得。SimpleAdapterでは1行分のデータはMap型!
-            Map<String, String> item = (Map<String, String>) parent.getItemAtPosition(position);
-            // 定食名と金額を取得。
-            String menuName = item.get("name");
-            String menuPrice = item.get("price");
+    @Override
+    public void onStart() {
+        Log.i("LifeCycleSample", "Main onStart() called.");
+        super.onStart();
+    }
 
-            // インテントオブジェクトを生成。
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+    @Override
+    public void onRestart() {
+        Log.i("LifeCycleSample", "Main onRestart() called.");
+        super.onRestart();
+    }
 
-            intent.putExtra("menuName", menuName);
-            intent.putExtra("menuPrice", menuPrice);
-            // 第2画面の起動。
-            startActivity(intent);
-        }
+    @Override
+    public void onResume() {
+        Log.i("LifeCycleSample", "Main onResume() called.");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.i("LifeCycleSample", "Main onPause() called.");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i("LifeCycleSample", "Main onStop() called.");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("LifeCycleSample", "Main onDestroy() called.");
+        super.onDestroy();
+    }
+
+    /**
+     * [次の画面を表示]ボタンがタップされたときの処理。
+     */
+    public void onButtonClick(View view) {
+// インテントオブジェクトを用意。
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        // アクティビティを起動。
+        startActivity(intent);
     }
 }
